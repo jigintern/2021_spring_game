0c5cb03b18c5705d84cb86db35f0ddb3bcb56942
Rikuto Sueta <rikutonsicloud@gmail.com>
2021-03-29 20:39:52 +0900

    Adjust mobile layout

--- board/header.php
+++ board/header.php
@@ -9,9 +9,19 @@
     <div class=header_title><a href="index.php">ゲームチャンネル</a></div>
     <div class="phone_area">
         <div class="header">
-            <div class=header_content>初心者向け</div>
-            <div class=header_content>掲示板</div>
-            <div class=header_content>カテゴリ別動画一覧</div>
+            <div class=header_content>
+                <a href="forbeginners.php">
+                    初心者向け
+                </a>
+            </div>
+            <div class=header_content>
+                掲示板
+            </div>
+            <div class=header_content>
+                <a href="categoriedmovies.php">
+                    カテゴリ別動画一覧
+                </a>
+            </div>
         </div>
     </div>
 </body>
